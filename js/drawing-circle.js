2d4e47ccaea8ca8984a83ecfae3ad144bf3830cf
wychen <ccwy1120@hotmail.com>
2020-04-22 14:40:27 +0800

    ' circle function complete, oval in progress'

--- js/drawing-circle.js
+++ js/drawing-circle.js
@@ -1,19 +1,63 @@
-$('#drawing-circle').click(()=>{
-    currentFunction = new DrawingCircle(contextReal,contextDraft);
+$('#drawing-circle').click(() => {
+    currentFunction = new DrawingCircle(contextReal, contextDraft);
 });
 
-class DrawingCircle extends PaintFunction{
-    constructor(contextReal){
+class DrawingCircle extends PaintFunction {
+    constructor(contextReal) {
         super();
-        this.context = contextReal;            
+        this.contextReal = contextReal;
+        this.contextDraft = contextDraft;
+        this.clickNum = 0
     }
-    
+    onMouseDown() { }
 
-    // let r = 50;  // radius
-    // context.arc(200, 200, r, 0, Math.PI * 2);  // draw
-    // context.fill();  // fill
-    // context.stroke();//no fill
+    onDragging() { }
 
+    onMouseMove(coord, event) {
 
+        this.contextDraft.strokeStyle = curStroke;
+        this.contextDraft.fillStyle = curFill;
+        this.contextDraft.lineWidth = curWidth;
 
+        if (this.clickNum != 0) {
+            this.contextDraft.clearRect(0, 0, canvasDraft.width, canvasDraft.height);
+            this.drawCenter(coord[0], coord[1], this.contextDraft);
+        }
+    }
+    onMouseUp(coord, event) {
+        this.contextReal.strokeStyle = curStroke;
+        this.contextReal.fillStyle = curFill;
+        this.contextReal.lineWidth = curWidth;
+
+        if (this.clickNum === 0) {
+            this.origX = coord[0];
+            this.origY = coord[1];
+            this.clickNum++;
+        }
+        else if (this.clickNum != 0) {
+            this.contextDraft.clearRect(0, 0, canvasDraft.width, canvasDraft.height);
+            this.drawCenter(coord[0], coord[1], this.contextReal);
+            this.clickNum = 0;
+        }
+    }
+
+    onMouseLeave() { }
+    onMouseEnter() { }
+
+    radiusCircle(x, y) {
+        let diffX = this.origX - x;
+        let diffY = this.origY - y;
+        let radiusSquare = Math.pow(diffX, 2) + Math.pow(diffY, 2);
+        let radius = Math.pow(radiusSquare, 0.5);
+        return radius;
+    }
+
+    drawCenter(x, y, context) {
+        context.beginPath();
+        let radius = this.radiusCircle(x, y)
+        context.arc(this.origX, this.origY, radius, 0, 2 * Math.PI);
+        // context.closePath();
+        context.fill();
+        context.stroke();
+    }
 }
